381e0fe8c08454a9fda33eeed59b3589483c1252
liang.he <lianhe@paypal.com>
2021-06-18 08:57:03 +0800

    update

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component{
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> an
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+
+export default App;
